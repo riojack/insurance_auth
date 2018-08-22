0f52b13e613397063a3e369c0b3f509949d6d725
riojack <ayjay122@gmail.com>
2018-08-21 19:35:38 -0500

    Update deps... trimming the fat

--- mix.exs
+++ mix.exs
@@ -27,8 +27,6 @@ defmodule InsuranceAuth.Mixfile do
     [
       {:phoenix, "~> 1.3.4"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"}
     ]
   end
